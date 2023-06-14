a562cfe05890a375e597766ca9cdca243915ad14
YareeYare <mehulshakya230@gmail.com>
2023-06-14 16:32:28 +0530

    encrypted user password using bcrypt

--- Code/Controllers/user.js
+++ Code/Controllers/user.js
@@ -1,24 +1,32 @@
 const user = require('../models/user')
 const { v4: uuidv4 } = require("uuid")
 const { SetUser } = require("../Service/auth");
+const { body , validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 async function CreateUser(req,resp){
 
+    const salt = await bcrypt.genSalt(10);
+    let secpassword = await bcrypt.hash(req.body.password,salt);
+    
     await user.create({
         name : req.body.name,
         email : req.body.email,
-        password : req.body.password
+        password : secpassword
     });
     return resp.redirect('/');
 }
 
 async function LoginUser(req,resp){
-    const credentials = {email,password} = req.body
-    const userdata = await user.findOne(credentials)
+    let email = req.body.email
+    let userdata = await user.findOne({email});
     if(!userdata){
-        return resp.render('Login',{
-            error : "Enter a valid Username and Password"
-        })
+        return resp.redirect('/signup')
+    }
+
+    const pwdcompare = await bcrypt.compare(req.body.password,userdata.password);
+    if(!pwdcompare){
+        return resp.redirect('/login')
     }
 
     const authToken = SetUser(userdata)
