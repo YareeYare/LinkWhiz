dc9f73e3f76087db104768670e5d86bd32a8b33d
YareeYare <mehulshakya230@gmail.com>
2023-06-20 18:00:51 +0530

    minor code adjustments for clarity

--- Code/Routes/url.js
+++ Code/Routes/url.js
@@ -1,6 +1,6 @@
 const express = require( 'express' )
 const router = express.Router()
-const { CreateShortURL } = require('../Controllers/url'); //, ViewsAndData
+const { CreateShortURL } = require('../Controllers/url');
 
 router.post('/' , CreateShortURL);
 module.exports = router;
